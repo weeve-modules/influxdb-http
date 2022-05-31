ad7ea5c0a537b5fb3b4c59bbd24501c06b158912
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-05-31 15:45:53 +0200

    run as standalone to be default

--- image/src/config/config.js
+++ image/src/config/config.js
@@ -6,8 +6,11 @@ module.exports = {
   MODULE_NAME: env('MODULE_NAME', 'influxdb-http'),
   EGRESS_URL: env('EGRESS_URL', ''),
   INFLUXDB_URL: env('INFLUXDB_URL', ''),
-  INFLUXDB_API_KEY: env('INFLUXDB_API_KEY', ''),
+  INFLUXDB_API_KEY: env(
+    'INFLUXDB_API_KEY',
+    ''
+  ),
   INFLUXDB_ORG: env('INFLUXDB_ORG', 'weeve'),
   INFLUXDB_BUCKET: env('INFLUXDB_BUCKET', 'testmp'),
-  RUN_AS_STANDALONE: env('RUN_AS_STANDALONE', 'no'),
+  RUN_AS_STANDALONE: env('RUN_AS_STANDALONE', 'yes'),
 }
